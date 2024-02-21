c9d3c0a60b6368dc366fcbc8d1c800635a7fc28b
Sergey <belykov1999@yandex.ru>
2024-02-21 17:56:08 +0300

    Add Home_Seminar_04:Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,51 @@
+﻿void Main()
+{
+
+
+    int arraySize = ReadInt("Введите количество чисел в массиве: ");
+    int[] array = GenerateArray(arraySize, 100, 999);
+    PrintArray(array);
+    FindCount(array);
+
+
+
+    int ReadInt(string msg)
+    {
+        System.Console.Write(msg);
+        return Convert.ToInt32(Console.ReadLine());
+    }
+
+    int[] GenerateArray(int size, int leftRange, int rightRange)
+    {
+        int[] myArray = new int[size];
+
+        for (int i = 0; i < myArray.Length; i++)
+        {
+            myArray[i] = new Random().Next(leftRange, rightRange); //от 100 до 999;
+        }
+
+        return myArray;
+    }
+
+    void PrintArray(int[] arrayForPrint)
+    {
+        for (int i = 0; i < arrayForPrint.Length; i++)
+        {
+            System.Console.Write(arrayForPrint[i] + " ");
+        }
+    }
+    void FindCount(int[] arrayCount)
+    {
+        int count = 0;
+        for (int i = 0; i < arrayCount.Length; i++)
+        {
+            if (arrayCount[i] % 2 == 0)
+            {
+                count++;
+            }
+        }
+        System.Console.WriteLine("\n" + count);
+    }
+
+}
+Main();
